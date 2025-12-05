08ac82fc7d1e3672217444ea9db3c5af18e3d65a
z-ai-ev <zev.destroopereisens@kavoskenslaan.be>
2025-12-05 20:58:52 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -408,7 +408,7 @@
                 } else {
                     element.textContent = current;
                 }
-            }, 8);
+            }, 450);
         }
 
         // Intersection Observer for stats animation
@@ -468,6 +468,7 @@
                 parallax.style.transform = `translateY(${scrolled * 0.5}px)`;
             }
         });
+
 
 
 
